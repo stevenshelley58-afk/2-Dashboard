bf1ce979932c96ac67666b88a9cba7a2b1b72bd5
stevenshelley58-afk <stevenshelley58@gmail.com>
2025-11-19 10:38:31 +0800

    fix: correct locals access in root page load

--- apps/web-svelte/src/routes/+page.server.ts
+++ apps/web-svelte/src/routes/+page.server.ts
@@ -1,8 +1,8 @@
 import { redirect } from '@sveltejs/kit'
 import type { PageServerLoad } from './$types'
 
-export const load: PageServerLoad = async ({ locals: { user } }) => {
-    if (user) {
+export const load: PageServerLoad = async ({ locals }) => {
+    if (locals.user) {
         throw redirect(303, '/dashboard')
     } else {
         throw redirect(303, '/login')
